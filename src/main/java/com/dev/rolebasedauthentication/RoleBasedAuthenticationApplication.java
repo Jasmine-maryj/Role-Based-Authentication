bd7e14fcf0ac95d6ca09887a42622b80f06cfc6f
Jasmine Mary <jasminemary630@gmail.com>
2023-08-13 23:39:17 +0530

    basic configurations added

---
+++ src/main/java/com/dev/rolebasedauthentication/RoleBasedAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package com.dev.rolebasedauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleBasedAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoleBasedAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/rolebasedauthentication/UserBasedAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.rolebasedauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserBasedAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
